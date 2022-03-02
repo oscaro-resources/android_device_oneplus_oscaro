811b02f1ed629206ff8b29870d9630816abfdb82
Alexander Koskovich <akoskovich@pm.me>
2022-03-02 13:41:26 -0700

    oscaro: Set USB product string to 'OnePlus Nord CE 2 Lite 5G'
    * This is better for user facing stuff than 'CPH2381'.
    
    Change-Id: I1f380699692c07b2fcc03fa83adc292b6d899cf7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,5 +13,8 @@ DEVICE_PATH := device/oneplus/oscaro
 BOARD_ONEPLUS_DYNAMIC_PARTITIONS_SIZE := 11186208768
 BOARD_SUPER_PARTITION_SIZE := 11190403072
 
+# Properties
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
 # Inherit the proprietary files
 -include vendor/oneplus/oscaro/BoardConfigVendor.mk
