f5e36bd747e5f35180f5ccc438da289be3d9d007
Vivekachooz <vivekachooz@gmail.com>
2025-05-25 20:55:57 +0530

    oscaro: Update from CPH2381_14.0.0.2000(EX01)
    Change-Id: I76353dc2d66877d5ac085a1dbaec0158d6c2dbab

--- lineage_oscaro.mk
+++ lineage_oscaro.mk
@@ -25,8 +25,8 @@ PRODUCT_MANUFACTURER := oneplus
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="OP535DL1-user 14 UKQ1.230924.001 S.1cb6762-1-22eaa release-keys" \
-    BuildFingerprint=OnePlus/CPH2381/OP535DL1:14/UKQ1.230924.001/S.1cb6762-1-22eaa:user/release-keys \
+    BuildDesc="OP535DL1-user 14 UKQ1.230924.001 S.1da1b14-1829a-36c83 release-keys" \
+    BuildFingerprint=OnePlus/CPH2381/OP535DL1:14/UKQ1.230924.001/S.1da1b14-1829a-36c83:user/release-keys \
     DeviceName=OP535DL1 \
     DeviceProduct=OP535DL1 \
     SystemDevice=OP535DL1 \
