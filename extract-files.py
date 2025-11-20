77163e6a8a71b09c8ab24a7b16ff171d1f6ec48c
Michael Bestas <mkbestas@lineageos.org>
2025-11-21 01:43:15 +0200

    oscaro: Depend on android.hardware.graphics.common-V7
    Required for BP3A.
    
    Change-Id: Id071b20aa56d5c2e2717215fa6bb6f16f91198ec

--- extract-files.py
+++ extract-files.py
@@ -24,7 +24,7 @@ namespace_imports = [
 
 blob_fixups: blob_fixups_user_type = {
     'odm/lib64/libAlgoProcess.so': blob_fixup()
-        .replace_needed('android.hardware.graphics.common-V1-ndk_platform.so', 'android.hardware.graphics.common-V6-ndk.so'),
+        .replace_needed('android.hardware.graphics.common-V1-ndk_platform.so', 'android.hardware.graphics.common-V7-ndk.so'),
     ('odm/lib/liblvimfs_wrapper.so', 'odm/lib64/libCOppLceTonemapAPI.so', 'odm/lib64/libaps_frame_registration.so', 'odm/lib64/libYTCommon.so'): blob_fixup()
         .replace_needed('libstdc++.so', 'libstdc++_vendor.so')
 }  # fmt: skip
