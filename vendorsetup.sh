c57aa70d8f7922ab58368d19671bd298885fbc46
NoCache-69 <chethangowdasj86@gmail.com>
2026-01-13 06:07:34 +0000

    oscaro: Include OOS camera - Drop prebuilt LMC cam
    Signed-off-by: NoCache-69 <chethangowdasj86@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,3 +6,4 @@ git clone https://github.com/oscaro-resources/hardware_oplus.git -b 16.2 hardwar
 git clone https://github.com/oscaro-resources/android_kernel_oneplus_sm6375.git -b 16.2 kernel/oneplus/sm6375 --depth=1
 git clone https://github.com/oscaro-resources/hardware_dolby.git -b lunaris-dolby hardware/dolby
 git clone https://github.com/AxionAOSP/android_packages_apps_ViPER4AndroidFX.git -b v4a packages/apps/ViPER4AndroidFX
+git clone https://gitlab.com/NoCache-69/proprietary_vendor_oplus_camera.git -b lineage-23.1 vendor/oplus/camera
