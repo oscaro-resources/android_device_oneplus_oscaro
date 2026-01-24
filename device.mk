3395d92a8359401b5947da30b4feebd9bab804cd
NoCache-69 <chethangowdasj86@gmail.com>
2026-01-24 13:55:27 +0000

    oscaro: Add RemovePackages to debloat unwanted apps
    Signed-off-by: NoCache-69 <chethangowdasj86@gmail.com>

--- device.mk
+++ device.mk
@@ -50,6 +50,10 @@ PRODUCT_PACKAGES += \
     OPlusFrameworksResTarget \
     OPlusSettingsProviderResTarget
 
+# RemovePackages
+ PRODUCT_PACKAGES += \
+     RemovePackagesOscaro
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
