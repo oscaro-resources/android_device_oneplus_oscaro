ec6d990785fbbfe59b01db74758b59c1a2e8fcd0
NoCache-69 <chethangowdasj86@gmail.com>
2026-04-04 12:45:24 +0530

    oscaro: Drop vendorsetup.sh for Axion-CI

--- vendorsetup.sh
+++
@@ -1,8 +0,0 @@
-# Clone Device repos
-git clone https://github.com/oscaro-resources/proprietary_vendor_oneplus_oscaro.git -b 16.2 vendor/oneplus/oscaro
-git clone https://github.com/oscaro-resources/hardware_oplus.git -b 16.2 hardware/oplus
-git clone https://github.com/oscaro-resources/android_kernel_oneplus_sm6375.git -b 16.2 kernel/oneplus/sm6375 --depth=1
-git clone https://github.com/oscaro-resources/hardware_dolby.git -b lunaris-dolby hardware/dolby
-git clone https://github.com/AxionAOSP/android_packages_apps_ViPER4AndroidFX.git -b v4a packages/apps/ViPER4AndroidFX
-git clone https://gitlab.com/NoCache-69/proprietary_vendor_oplus_camera.git -b lineage-23.1 vendor/oplus/camera
-rm -rf vendor/lineage-priv/keys && git clone https://github.com/oscaro-resources/axion_keys.git vendor/lineage-priv/keys
