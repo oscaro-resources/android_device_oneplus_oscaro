1bdfba5f8f17264a3070f61f8f06392fe5d05be5
NoCache-69 <chethangowdasj86@gmail.com>
2026-06-24 13:04:29 +0530

    vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/oscaro-resources/android_device_oneplus_sm6375-common  -b los device/oneplus/sm6375-common
+git clone https://github.com/oscaro-resources/hardware_oplus -b los hardware/oplus
+git clone https://github.com/oscaro-resources/android_kernel_oneplus_sm6375 -b 16.2 kernel/oneplus/sm6375
+git clone https://github.com/oscaro-resources/proprietary_vendor_oneplus_oscaro -b los vendor/oneplus/oscaro
+git clone https://github.com/oscaro-resources/proprietary_vendor_oneplus_sm6375-common -b los vendor/oneplus/sm6375-common
