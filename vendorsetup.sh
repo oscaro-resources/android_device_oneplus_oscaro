c0107e7cf5175aaf7b89e7653ed2374a22eaf40d
NoCache-69 <chethangowdasj86@gmail.com>
2026-07-04 11:21:27 +0000

    oscaro: vendorsetup: Track 17.0 branches - Also drop oplus cam

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,8 @@
 # Clone Device repos
-git clone https://github.com/oscaro-resources/android_device_oneplus_sm6375-common device/oneplus/sm6375-common
-git clone https://github.com/oscaro-resources/proprietary_vendor_oneplus_sm6375-common vendor/oneplus/sm6375-common
-git clone https://github.com/oscaro-resources/proprietary_vendor_oneplus_oscaro.git -b 16.2 vendor/oneplus/oscaro
+git clone https://github.com/oscaro-resources/android_device_oneplus_sm6375-common -b 17.0 device/oneplus/sm6375-common
+git clone https://github.com/oscaro-resources/proprietary_vendor_oneplus_sm6375-common -b 17.0 vendor/oneplus/sm6375-common
+git clone https://github.com/oscaro-resources/proprietary_vendor_oneplus_oscaro.git -b 17.0 vendor/oneplus/oscaro
 git clone https://github.com/oscaro-resources/hardware_oplus.git -b 16.2 hardware/oplus
-git clone https://github.com/oscaro-resources/android_kernel_oneplus_sm6375.git -b 16.2 kernel/oneplus/sm6375 --depth=1
+git clone https://github.com/oscaro-resources/android_kernel_oneplus_sm6375.git -b 17.0 kernel/oneplus/sm6375 --depth=1
 git clone https://github.com/oscaro-resources/hardware_dolby.git -b lunaris-dolby hardware/dolby
-git clone https://gitlab.com/NoCache-69/proprietary_vendor_oplus_camera.git -b lineage-23.1 vendor/oplus/camera
 git clone https://github.com/AxionAOSP/android_packages_apps_ViPER4AndroidFX.git -b v4a packages/apps/ViPER4AndroidFX
